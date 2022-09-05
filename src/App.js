009bbb3cfb4f7ea0d81f2fce923dd1fc1c3eaa18
Thomas Json <joogletom8@gmail.com>
2022-09-05 10:05:36 +0200

    E5

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import './App.css';
-import HomeScreen from './components/HomeScreen/HomeScreen';
 import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+
+import HomeScreen from './components/HomeScreen/HomeScreen';
+import ContactForm from './components/ContactForm/ContactForm';
 
 function App() {
   return (
     <>
       <HomeScreen />
+      <ContactForm />
     </>
   );
 }

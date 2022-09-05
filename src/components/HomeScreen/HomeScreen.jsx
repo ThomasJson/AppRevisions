f1af1b7270f5d049ea8fad98f19d717b7e22d3a6
Thomas Json <joogletom8@gmail.com>
2022-09-05 09:45:01 +0200

    E4

--- src/components/HomeScreen/HomeScreen.jsx
+++ src/components/HomeScreen/HomeScreen.jsx
@@ -5,13 +5,46 @@ import "./homeScreen.css";
 
 const HomeScreen = () => {
 
+  const cardCategories = [
+    {
+      id: "1",
+      title: "Terreur sous les tropiques",
+      description: 'something',
+      image: "./assets/images/johnny.jpeg",
+    },
+    {
+      id: "2",
+      title: "Les conquérants",
+      description: 'something',
+      image: "./assets/images/botez.jpg",
+    },
+    {
+      id: "3",
+      title: "Massacre à la sulfateuse",
+      description: 'something',
+      image: "https://picsum.photos/id/209",
+    },
+    {
+      id: "4",
+      title: `L'apprentie sorcière`,
+      description: 'something',
+      image: "https://i.picsum.photos/id/548",
+    },
+  ];
+
+  
 
   return (
     <>
-      <div>
+      <div className="col">
         <h1>La Boutique De Laurent Bedu</h1>
-        <CategoryCard title={"Johnny"} description={"L'homme aux 1000 métiers"} image={"./assets/images/johnny.jpeg"}/>
-        <CategoryCard title={"Andréa"} description={"Andréa ' Energy ' Botez"} image={"./assets/images/botez.jpg"}/>
+
+        <div className="row">
+          {cardCategories.map((card, index) => (
+            <div className="carD">
+              <CategoryCard key={index} card={card} title={card.title} description={card.description} image={card.image} />
+            </div>))}
+        </div>
       </div>
     </>
   );
